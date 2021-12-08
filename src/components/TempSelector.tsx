1504a2c0161e1fdab9ded9d2b66a4caf559f9690
teeliny <teeliny@gmail.com>
2021-12-08 13:15:13 +0100

    Added temp unit selector

---
+++ src/components/TempSelector.tsx
@@ -0,0 +1,29 @@
+import { FormControl, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import React from 'react';
+
+interface ITempSelector {
+  value: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>, value: string) => void;
+}
+
+function TempSelector({value, handleChange}: ITempSelector) {
+  return (
+    <>
+      <FormControl component="fieldset" sx={{ width: '100%' }}>
+        <RadioGroup
+          sx={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}
+          row={true}
+          aria-label="gender"
+          name="row-radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="0" control={<Radio />} label="Celsius" />
+          <FormControlLabel value="1" control={<Radio />} label="Fahrenheit" />
+        </RadioGroup>
+      </FormControl>
+    </>
+  )
+}
+
+export default TempSelector;
