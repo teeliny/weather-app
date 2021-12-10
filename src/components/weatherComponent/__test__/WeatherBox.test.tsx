05409226b984a2fd8346052ba175d1bbeb3e5128
teeliny <teeliny@gmail.com>
2021-12-10 19:28:43 +0100

    Added snapshot test for weather box

--- src/components/weatherComponent/__test__/WeatherBox.test.tsx
+++ src/components/weatherComponent/__test__/WeatherBox.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import WeatherBox from '../WeatherBox';
 
 afterEach(cleanup);
@@ -38,4 +39,21 @@ it('renders box correctly', () => {
     />
   );
   expect(getByTestId('single-box')).toHaveTextContent('Temperature');
-})
+});
+
+it('matches weather box snapshot', () => {
+  const tree = renderer
+    .create(
+      <WeatherBox
+        temp={30}
+        humidity={60}
+        current_date={''}
+        tempUnit={'0'}
+        id={''}
+        handleSelectDay={(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+          throw new Error('Function not implemented.');
+        }}
+      />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
